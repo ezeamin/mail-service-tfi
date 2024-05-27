95ab99c984ee3be6df27dd4fee4bbc0f53e4e6ce
Ezequiel Amin <ezequielamin@outlook.com>
2024-05-27 18:17:35 -0300

    improve logging and try except db connection

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy as _BaseSQLAlchemy
 from os import getenv
 from dotenv import load_dotenv
 
@@ -9,6 +9,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{getenv('PGUSER')}:{getenv('PGPASSWORD')}@{getenv('PGHOST')}:{getenv('PGPORT', 5432)}/{getenv('PGDATABASE')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+class SQLAlchemy(_BaseSQLAlchemy):
+    def apply_pool_defaults(self, app, options):
+        super(SQLAlchemy, self).apply_pool_defaults(self, app, options)
+        options["pool_pre_ping"] = True
+        
 db = SQLAlchemy(app)
 
 class Emails(db.Model):
@@ -26,7 +31,10 @@ def create_email(email_message,body,result):
 
     result_message = 'Phishing' if result == -1 else 'Safe'
     with app.app_context():
-        email = Emails(from_field=from_field, subject=subject, body=body, predicted=result_message, actual=actual)
-        db.session.add(email)
-        db.session.commit()
-        print(f"🧮 Email from {from_field} saved in the database.")
+        try:
+            email = Emails(from_field=from_field, subject=subject, body=body, predicted=result_message, actual=actual)
+            db.session.add(email)
+            db.session.commit()
+            print(f"\n🧮 Email from {from_field} saved in the database.\n")
+        except Exception as e:
+            print(f"❌ Error saving email in the database: {e}")
